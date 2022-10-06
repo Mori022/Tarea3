a36ecd5fd7c700b0e551ed8d8ea99d7207ca83c8
Mori022 <zx19student132@colegiovalledelmiro.es>
2022-10-06 12:38:01 +0200

    Resta

--- operacion.js
+++ operacion.js
@@ -2,6 +2,6 @@ function calcular(){
    salida = document.getElementById("salida");
    n1 = parseInt(document.getElementById("n1").value);
    n2 = parseInt(document.getElementById("n2").value);
-   n3 = n1 + n2;
+   n3 = n1 - n2;
    salida.innerHTML = n3;
 }
