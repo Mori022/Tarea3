4f8e72480e4b1447186264088594df77e2184a16
Mori022 <zx19student132@colegiovalledelmiro.es>
2022-10-07 09:57:35 +0200

    Potencias

--- operacion.js
+++ operacion.js
@@ -1,7 +1,6 @@
 function calcular(){
    salida = document.getElementById("salida");
-   n1 = parseInt(document.getElementById("n1").value);
-   n2 = parseInt(document.getElementById("n2").value);
-   n3 = n1 + n2;
-   salida.innerHTML = n3;
+   n3 = parseInt(document.getElementById("n3").value);
+   n4 = n3*n3;
+   salida.innerHTML = n4;
 }
